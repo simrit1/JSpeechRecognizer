21de95b53cb86fec4ea92c6442b32adc5608773d
matt <philmattdev@gmail.com>
2021-08-23 23:53:10 +0800

    stuff

--- setup.py
+++ setup.py
@@ -1,21 +1,16 @@
 from distutils.core import setup
 setup(
-    name='jspeechrecognizer',         # How you named your package folder (MyLib)
-    packages=['jspeechrecognizer'],   # Chose the same as "name"
-    version='0.1',      # Start with a small number and increase it with every change you make
-    # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+    name='jspeechrecognizer',
+    packages=['jspeechrecognizer'], 
+    version='0.1-beta', 
     license='apache-2.0',
-    # Give a short description about your library
     description='A Speech Recognition library that combines wakeword detection, speech recognition, and voice activity detection.',
-    author='Philippe Mathew',                   # Type in your name
-    author_email='philmattdev@gmail.com',      # Type in your E-Mail
-    # Provide either the link to your github or to your website
-    url='https://github.com/user/reponame',
-    # I explain this later on
-    download_url='https://github.com/user/reponame/archive/v_01.tar.gz',
-    # Keywords that define your package best
+    author='Philippe Mathew',
+    author_email='philmattdev@gmail.com',
+    url='https://github.com/bossauh/JSpeechRecognizer',
+    download_url='https://github.com/bossauh/JSpeechRecognizer/archive/refs/tags/v_0.1-beta.tar.gz',
     keywords=['speech', 'recognition', 'vad', 'wakeword'],
-    install_requires=[            # I get to this in a second
+    install_requires=[
         'pvporcupine',
         'vosk',
         'speechrecognition',
@@ -25,15 +20,10 @@ setup(
         'numpy'
     ],
     classifiers=[
-        # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-        'Development Status :: 4 - Beta',
-        # Define that your audience are developers
+        'Development Status :: 4 - Beta', # 3=Alpha | 4=Beta | 5=Production
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Build Tools',
-        'License :: OSI Approved :: Apache 2.0 License',   # Again, pick a license
-        # Specify which python versions that you want to support
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
+        'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
     ],
